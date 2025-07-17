5dbd36857434e1a62084720b46701ae14be71cac
moonjin-kim <ace39418@gmail.com>
2025-07-17 22:26:47 +0900

    feat: 포인트 충전 API 추가

---
+++ apps/commerce-api/src/main/java/com/loopers/interfaces/api/point/PointV1ApiSpec.java
@@ -0,0 +1,15 @@
+package com.loopers.interfaces.api.point;
+
+import com.loopers.interfaces.api.ApiResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+@Tag(name = "POINT V1 API", description = "POINT API 입니다.")
+public interface PointV1ApiSpec {
+    @Operation(
+            summary = "포인트 충전",
+            description = "현재 유저에 amont만큼의 포인트를 추가한다."
+    )
+    ApiResponse<PointV1ResponseDto.PointBalanceResponse> register(String account, PointV1RequestDto.PointChargeRequest chargeRequest);
+
+}
