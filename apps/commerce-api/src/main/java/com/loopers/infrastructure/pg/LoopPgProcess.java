2ce03d932cef55d9ec2ed4ac8dc0615a655a18b0
moonjin-kim <ace39418@gmail.com>
2025-08-23 15:32:44 +0900

    feat: 결제 PG에 fallback 추가

--- apps/commerce-api/src/main/java/com/loopers/infrastructure/pg/LoopPgProcess.java
+++ apps/commerce-api/src/main/java/com/loopers/infrastructure/pg/LoopPgProcess.java
@@ -2,6 +2,8 @@ package com.loopers.infrastructure.pg;
 
 import com.loopers.domain.pg.PgProcess;
 import com.loopers.infrastructure.payment.PaymentResponse;
+import com.loopers.support.error.CoreException;
+import com.loopers.support.error.ErrorType;
 import feign.FeignException;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import io.github.resilience4j.retry.annotation.Retry;
@@ -17,14 +19,14 @@ import java.util.List;
 public class LoopPgProcess implements PgProcess {
     private final LoopFeignClient pgFeignClient;
 
-    @CircuitBreaker(name = "pgCircuit")
+    @CircuitBreaker(name = "pgCircuit", fallbackMethod = "fallbackPayment")
     @Retry(name = "pgRetry")
     @Override
     public PgResponse.Pay payment(PgRequest.Pay request, Long userId) {
         return pgFeignClient.payment(request, userId).getData();
     }
 
-    @CircuitBreaker(name = "pgCircuit")
+    @CircuitBreaker(name = "pgCircuit", fallbackMethod = "fallbackPayment")
     @Retry(name = "pgRetry")
     @Override
     public PgResponse.FindByOrderId findByOrderId(String orderId, Long userId) {
@@ -36,11 +38,16 @@ public class LoopPgProcess implements PgProcess {
         }
     }
 
-    @CircuitBreaker(name = "pgCircuit")
+    @CircuitBreaker(name = "pgCircuit", fallbackMethod = "fallbackPayment")
     @Retry(name = "pgRetry")
     @Override
     public PgResponse.Find findByPGId(String paymentId, Long userId) {
         PaymentResponse<PgResponse.Find> result = pgFeignClient.findByPaymentId(paymentId, userId);
         return result.getData();
+    }
+
+    private PgResponse.Pay fallbackPayment(PgRequest.Pay request, Long userId, Throwable throwable) {
+        log.error("PG 결제 요청 실패: {}", throwable.getMessage());
+        throw new CoreException(ErrorType.PG_ERROR, "PG 결제 요청에 실패했습니다. 잠시 후 다시 시도해주세요.");
     }
 }
