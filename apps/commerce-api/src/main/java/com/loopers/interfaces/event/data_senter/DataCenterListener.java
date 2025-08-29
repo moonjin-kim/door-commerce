5054f14ac48e8a7caac2eb6cfcd4185863aea593
moonjin-kim <ace39418@gmail.com>
2025-08-29 09:45:25 +0900

    feat: 유저 결제, 좋아요 이벤트 데이터 플랫폼에서 체크

--- apps/commerce-api/src/main/java/com/loopers/interfaces/event/data_senter/DataCenterListener.java
+++ apps/commerce-api/src/main/java/com/loopers/interfaces/event/data_senter/DataCenterListener.java
@@ -1,7 +1,9 @@
 package com.loopers.interfaces.event.data_senter;
 
+import com.loopers.domain.like.LikeEvent;
 import com.loopers.domain.order.OrderEvent;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.event.EventListener;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.event.TransactionPhase;
@@ -11,14 +13,33 @@ import org.springframework.transaction.event.TransactionalEventListener;
 @Slf4j
 public class DataCenterListener {
     @Async
-    @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
+    @EventListener
     void handle(OrderEvent.Complete event) {
         log.info("Order complete event: {}", event);
     }
 
     @Async
-    @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
+    @EventListener
     void handle(OrderEvent.Cancel event) {
         log.info("Order Cancel event: {}", event);
     }
+
+    @Async
+    @EventListener
+    void handle(OrderEvent.RequestPayment event) {
+        log.info("결제 요청 {}", event);
+    }
+
+    @Async
+    @EventListener
+    void handle(LikeEvent.Like event) {
+        log.info("유저의 좋아요: {}", event);
+    }
+
+    @Async
+    @EventListener
+    void handle(LikeEvent.UnLike event) {
+        log.info("유저의 좋아요 취소: {}", event);
+    }
+
 }
