3319e463b26551d931cc9f3a68a76712d7c56b8d
moonjin-kim <ace39418@gmail.com>
2025-09-12 12:45:59 +0900

    mod: 일간 랭킹 TTL 수정

--- apps/commerce-streamer/src/main/java/com/loopers/infrastructure/ranking/RankingRedisRepository.java
+++ apps/commerce-streamer/src/main/java/com/loopers/infrastructure/ranking/RankingRedisRepository.java
@@ -51,7 +51,7 @@ public class RankingRedisRepository implements RankingRepository {
         redisTemplate.opsForZSet()
                 .unionAndStore(todayKey, Collections.emptyList(), tomorrowKey, Aggregate.SUM, Weights.of(weight));
         // TTL 부여 (예: 30일 후 삭제)
-        redisTemplate.expire(tomorrowKey, Duration.ofDays(30));
+        redisTemplate.expire(tomorrowKey, Duration.ofDays(2));
     }
 
     @Override
