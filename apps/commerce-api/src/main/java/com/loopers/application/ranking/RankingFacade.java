88a5cab3d660ff4732845f27ca1fc4634349ebdf
moonjin-kim <ace39418@gmail.com>
2025-09-14 23:40:37 +0900

    mod: 랭킹 조회시 랭키에 포함되는 상품이 아닐경우 스킵하도록 수정

--- apps/commerce-api/src/main/java/com/loopers/application/ranking/RankingFacade.java
+++ apps/commerce-api/src/main/java/com/loopers/application/ranking/RankingFacade.java
@@ -13,6 +13,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.Set;
 
 @Component
@@ -24,13 +25,14 @@ public class RankingFacade {
     public PageResponse<RankingV1Response.ProductDto> getRanking(String date, int page, int size) {
         List<String> rankingPage = rankingService.getRanking(RankingCommand.GetRanking.of(date, page, size)).stream().toList();
 
-        List<RankingV1Response.ProductDto> products = rankingPage.stream().map(rank -> {
-            Long productId = Long.valueOf(rank);
-            Product product = productService.getBy(productId).orElseThrow(() ->
-                    new CoreException(ErrorType.NOT_FOUND, "Product not found for ID: " + productId)
-            );
-            return RankingV1Response.ProductDto.from(product);
-        }).toList();
+        List<RankingV1Response.ProductDto> products = rankingPage.stream()
+                .map(Long::valueOf)
+                .map(productId -> productService.getBy(productId)
+                        .map(RankingV1Response.ProductDto::from)
+                        .orElse(null)
+                )
+                .filter(Objects::nonNull)
+                .toList();
 
         return PageResponse.of(page,size, products);
     }
